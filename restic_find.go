9edbdad4bf423de8c1f3e6261c99a140789a31f5
Sam Mobach <hello@sammobach.com>
2025-02-27 12:04:44 +0100

    chore: add restic find options

--- restic_find.go
+++ restic_find.go
@@ -5,10 +5,31 @@ import (
 	"strings"
 )
 
-func (r *Restic) Find(pattern string) ([]map[string]interface{}, error) {
+type FindOptFunc func(*FindOpts)
+
+type FindOpts struct {
+	snapshotId string
+}
+
+func FindWithSnapshotId(snapshotId string) FindOptFunc {
+	return func(o *FindOpts) {
+		o.snapshotId = snapshotId
+	}
+}
+
+func (r *Restic) Find(pattern string, opts ...FindOptFunc) ([]map[string]interface{}, error) {
+	o := FindOpts{}
+	for _, opt := range opts {
+		opt(&o)
+	}
+
 	cmd := []string{r.command}
 	cmd = append(cmd, r.flags...)
 	cmd = append(cmd, "find", pattern)
+
+	if len(o.snapshotId) > 0 {
+		cmd = append(cmd, "-s", o.snapshotId)
+	}
 
 	resultRaw, err := execute(cmd)
 	if err != nil {
